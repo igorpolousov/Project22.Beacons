4581d67a4182b547a7071429423093df0c0214f2
Igor Polousov <igorpolousov@gmail.com>
2021-12-20 12:53:20 +0300

    Initial Commit

---
+++ Project22.Beacons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22.Beacons
+//
+//  Created by Igor Polousov on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
