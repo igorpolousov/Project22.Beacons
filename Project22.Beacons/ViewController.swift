fc521badaaa20cdce4df5f5e5d0fd1974c240f83
Igor Polousov <igorpolousov@gmail.com>
2022-01-06 17:18:24 +0300

    + task 2

--- Project22.Beacons/ViewController.swift
+++ Project22.Beacons/ViewController.swift
@@ -11,12 +11,15 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     
+    @IBOutlet var deviceNameLabel: UILabel!
     // Надпись на экране
     @IBOutlet var distanceReading: UILabel!
     // Менеджер положения: начинает и останавливает предоставлениеданных о местоположении
     var locationManager: CLLocationManager?
     //переменная для определения найден маяк или нет
     var isDetected = false
+    
+    var currentBeaconUuid: UUID?
     
     
     override func viewDidLoad() {
@@ -29,6 +32,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager?.requestAlwaysAuthorization()
         // Сделан серый фон
         view.backgroundColor = .gray
+        // начальное название устройства
+        deviceNameLabel.text = "Device not detected"
     }
 
     // Проверка авторизации и возможностей сканирования устройством
@@ -47,37 +52,48 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     // Поиск устройства с заданными характеристиками
-    func startScanning() {
-        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
-        let beaconRegion = CLBeaconRegion(uuid: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+    func startScanning(major:UInt16 = 123, minor:UInt16 = 456) {
+        addBeaconRegion(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", major: major, minor: minor, identifier: "iPad")
+        addBeaconRegion(uuidString: "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6", major: major, minor: minor, identifier: "Radius Networks")
+        addBeaconRegion(uuidString: "92AB49BE-4127-42F4-B532-90fAF1E26491", major: major, minor: minor, identifier: "TwoCanoes")
+    }
+    
+    // Добавление маяка
+    func addBeaconRegion(uuidString: String, major: CLBeaconMajorValue, minor: CLBeaconMinorValue, identifier: String) {
+        let uuid = UUID(uuidString: uuidString)!
+        let beaconRegion = CLBeaconRegion(uuid: uuid, major: major, minor: minor, identifier: identifier)
         
         locationManager?.startMonitoring(for: beaconRegion)
         locationManager?.startRangingBeacons(in: beaconRegion)
     }
     
     // При обнаружении устройства меняет цвет фона и надпись на экране
-    func update(distance: CLProximity) {
+    func update(distance: CLProximity, name: String) {
         UIView.animate(withDuration: 1) {
+           
             switch distance {
             case .far:
                 self.alertController()
                 self.view.backgroundColor = .blue
                 self.distanceReading.text = "FAR"
+                self.deviceNameLabel.text = "\(name)"
                 self.isDetected = true
             case .near:
                 self.alertController()
                 self.view.backgroundColor = .orange
                 self.distanceReading.text = "NEAR"
+                self.deviceNameLabel.text = "\(name)"
                 self.isDetected = true
             case .immediate:
                 self.alertController()
                 self.view.backgroundColor = .red
                 self.distanceReading.text = "RIGHT HERE"
+                self.deviceNameLabel.text = "\(name)"
                 self.isDetected = true
-                
            default:
                 self.view.backgroundColor = .gray
                 self.distanceReading.text = "UNKNOWN"
+                self.deviceNameLabel.text = "\(name)"
                 self.isDetected = false
             }
         }
@@ -86,9 +102,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     // Нужна для обнаружения всех маяков в радиусе действия и добавляет их в массив в случае обнаружения
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
         if let beacon = beacons.first {
-            update(distance: beacon.proximity)
+            update(distance: beacon.proximity, name: region.identifier)
+            
+            if currentBeaconUuid == nil { currentBeaconUuid = region.proximityUUID }
+            guard currentBeaconUuid == region.proximityUUID else { return }
+            
         } else {
-            update(distance: .unknown)
+            guard currentBeaconUuid == region.proximityUUID else { return }
+            currentBeaconUuid = nil
+            update(distance: .unknown, name: "Device not detected")
         }
     }
     
